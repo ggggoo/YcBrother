d7dd9a427833945c3294d42a1d958036b445c25e
杨大爷 <1320011272@qq.com>
2017-04-26 19:01:12 +0800

    测试

--- app/src/main/java/com/landicorp/ycbrother/wifiborken/InstructionsClass.java
+++ app/src/main/java/com/landicorp/ycbrother/wifiborken/InstructionsClass.java
@@ -22,6 +22,8 @@ public class InstructionsClass extends Activity {
                         .setFontAttrId(R.attr.fontPath)
                         .build()
         );
+
+        
         setContentView(R.layout.instruction_layout);
         TextView instruction = (TextView) findViewById(R.id.textView4);
         instruction.setText("This is the first application of its kind that I have seen. There is no need to root the device. The app will try 10,000 most common passwords on the access point and will let you know if it is able to crack the password. Here are a few more instructions.\n\n1. Try turning wifi on before cracking process begins.\n2.The signal strength of the access point should be good.\n3.Try cracking passwords of access points which have WPA/WPA2/WEP security.\n4.Contact fsecurify@gmail.com in case of any problem. We are always here to help.");
